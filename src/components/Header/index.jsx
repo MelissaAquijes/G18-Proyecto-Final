e5347368200c362382632f36972a3c882508fbbc
Melissa Aquijes <melissa.geral10@gmail.com>
2024-05-14 00:46:40 -0500

    Correción de carrito.

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,8 +1,12 @@
 import { useState, useEffect } from "react";
 import logo from "../../assets/logo.png";
 
-export default function Header({ count, setCountUser, productsCart, setProductsCart }){
-
+export default function Header({
+  count,
+  setCountUser,
+  productsCart,
+  setProductsCart,
+}) {
   const [active, setActive] = useState(false);
   const [totalPrice, setTotalPrice] = useState(0);
 
@@ -13,7 +17,10 @@ export default function Header({ count, setCountUser, productsCart, setProductsC
   };
 
   const calculateTotalPrice = () => {
-    const total = productsCart.reduce((accumulator, product) => accumulator + product.price, 0);
+    const total = productsCart.reduce(
+      (accumulator, product) => accumulator + product.price,
+      0
+    );
     setTotalPrice(total);
   };
 
@@ -24,7 +31,7 @@ export default function Header({ count, setCountUser, productsCart, setProductsC
 
   return (
     <>
-      <header className="bg-white w-full">
+      <header className="bg-white w-full relative">
         <p className="bg-blue text-white p-2 text-center text-sm">
           Envíos gratis a Lima y Provincia por compras superiores a S/120.
         </p>
@@ -42,55 +49,70 @@ export default function Header({ count, setCountUser, productsCart, setProductsC
               className="text-blue relative ml-4 fa-solid fa-cart-shopping fa-lg cursor-pointer"
               onClick={() => setActive(!active)}
             >
-              <span className="bg-pink rounded-full w-4 text-center text-xs text-white absolute bottom-1 left-3">{count}</span>
+              <span className="bg-pink rounded-full w-4 text-center text-xs text-white absolute bottom-1 left-3">
+                {count}
+              </span>
             </i>
 
             <div
               className={`${
                 active
                   ? "hidden"
-                  : "bg-white w-[850px]  right-0  overflow-hidden fixed inset-y-0 max-w-full pl-10"
+                  : "bg-white w-[550px]  right-0 vh-full absolute"
               }`}
             >
-            <div className="flex justify-between p-4">
-              <h3>Mis compras</h3>
-              <i class="fa-solid fa-xmark"></i>
-            </div>
+              <div className="flex justify-between p-4">
+                <h3 className="font-bold">Mis compras</h3>
+                <i
+                  class="fa-solid fa-xmark cursor-pointer"
+                  onClick={() => setActive(!active)}
+                ></i>
+              </div>
 
-            <ul className="-my-6 divide-y divide-gray-200 mt-4">
-            {productsCart.map((product, index) => (
-              <li className="flex py-6">
-                <div
-                  key={index}
-                  className="w-full h-28 mb-2 flex justify-between"
-                >
-                  <img
-                    className="h-full rounded-md border border-gray-200"
-                    src={product.image}
-                    alt={product.title}
-                  />
-
-                  <div className="flex flex-col justify-between text-right items-end">
-                    <button
-                      className=""
-                      onClick={() => removeProductFromCart(index)}
+              <ul className="-my-6 divide-y divide-gray-200 mt-4">
+                {productsCart.map((product, index) => (
+                  <li className="flex py-6">
+                    <div
+                      key={index}
+                      className="w-full h-28 mb-2 flex justify-between"
                     >
-                      <i class="fa-solid fa-trash-can"></i>
-                    </button>
-                    <div>
-                      <p>{product.title}</p>
-                      <span>S/.{product.price}</span>
+                      <img
+                        className="h-full rounded-md border border-gray-200"
+                        src={product.image}
+                        alt={product.title}
+                      />
+
+                      <div className="flex flex-col justify-between text-right items-end">
+                        <div className="text-base font-medium text-gray-900">
+                          <p>{product.title}</p>
+                          <p>S/.{product.price}</p>
+                        </div>
+
+                        <button
+                          className=""
+                          onClick={() => removeProductFromCart(index)}
+                        >
+                          <i className="text-pink fa-solid fa-trash-can"></i>
+                        </button>
+                      </div>
                     </div>
-                  </div>
-                </div>
-              </li>
-                
-              ))}
-            </ul>
-              
-              <span className="font-bold">TOTAL: S/.{totalPrice}</span>
-              <button>Pagar</button>
+                  </li>
+                ))}
+              </ul>
+
+              <div className="flex justify-between border-t font-medium text-gray-900 border-gray-200 px-4 py-6 sm:px-6">
+                <p>Total</p>
+                <p> S/.{totalPrice}</p>
+              </div>
+
+              <a
+                className="flex items-center justify-center rounded-md border border-transparent bg-blue px-6 py-3 font-medium text-white shadow-sm hover:bg-pink"
+                src="#"
+              >
+                Pagar
+              </a>
             </div>
+
           </div>
         </div>
       </header>
