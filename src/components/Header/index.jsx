e3420015e346522d9aa814ca5482ab58fc0e0565
Dayvid Ramos <d.ramosm97@gmail.com>
2024-07-21 08:55:34 -0500

    fix: Open page without deployed boxes. Add close function to cart button.

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,30 +1,32 @@
 import { useState, useEffect } from "react";
 import logo from "../../assets/logo.png";
 
-export default function Header({ count, setCountUser, productsCart, setProductsCart, openUserModal }){
+export default function Header(props){
 
-  const [active, setActive] = useState(false);
+  // const [active, setActive] = useState(false);
   const [totalPrice, setTotalPrice] = useState(0);
 
   const removeProductFromCart = (index) => {
-    const updatedCart = productsCart.filter((_, i) => i !== index); //metodo filter primer parametro ignoramos
-    setProductsCart(updatedCart);
-    setCountUser(count - 1);
+    const updatedCart = props.productsCart.filter((_, i) => i !== index); //metodo filter primer parametro ignoramos
+    props.setProductsCart(updatedCart);
+    props.setCountUser(props.count - 1);
   };
 
   const calculateTotalPrice = () => {
-    const total = productsCart.reduce((accumulator, product) => accumulator + product.price, 0);
+    const total = props.productsCart.reduce((accumulator, product) => accumulator + product.price, 0);
     setTotalPrice(total);
   };
-
-  const closeCart = () => {
-
-  }
 
   useEffect(() => {
     //usamos UseEffect para que automaticamente corra la funcion cuando algo se modifica en [productCart]
     calculateTotalPrice();
-  }, [productsCart]);
+  }, [props.productsCart]);
+
+
+  // Status:   True = Open  -  False = Close
+  var [cartStatus, setCartStatus] = useState(false);
+  const openCart = () => setCartStatus(true);
+  const closeCart = () => setCartStatus(false);
 
   return (
     <>
@@ -42,36 +44,42 @@ export default function Header({ count, setCountUser, productsCart, setProductsC
           </a>
 
           <div className="relative flex w-[100px] h-[40px]">
+            
             {/* user icon*/}
             <div className="w-1/2 flex justify-center items-center">
-              <button onClick={openUserModal}>
+              <button onClick={props.openUserModal}>
                 <i className="text-blue fa-regular fa-user fa-lg"></i>
               </button>
             </div>
 
+
+
+
             {/* cart icon*/}
             <div className="w-1/2 flex justify-center items-center">
-              <button onClick={() => setActive(!active)}>
+              <button onClick={openCart}>
                 <i className="text-blue relative fa-solid fa-cart-shopping fa-lg">
-                  <span className="bg-pink rounded-full w-4 text-center text-xs text-white absolute bottom-1 left-3">{count}</span>
+                  <span className="bg-pink rounded-full w-4 text-center text-xs text-white absolute bottom-1 left-3">{props.count}</span>
                 </i>
               </button>
             </div>
 
+
             {/* cart desplegado */}
-            <div className={`${active? "hidden" : "bg-white w-[850px]  right-8 top-7 overflow-hidden absolute"}`}>
+            <div className={`bg-white w-[850px]  right-8 top-7 overflow-hidden absolute ${cartStatus? "" : "hidden"}`}>
+
               <div className="flex justify-between p-4 border-4 border-black">
                 <h3>Mis compras</h3>
-                {/* botón cerrar del cart desplegado */}
-                <button className="w-8 h-8 p-1 rounded-2xl bg-slate-200 hover:bg-bpink flex items-center justify-center"
-                        onClick={() => setActive(!active)}>
+                {/* botón cerrar del cart desplegado  bg-slate-200*/}
+                <button className="w-8 h-8 p-1 rounded-2xl bg-bpink hover:bg-pink flex items-center justify-center"
+                        onClick={closeCart}>
                   <i className="fa-solid fa-xmark"></i>
                 </button>
               </div>
 
               <div>
                 <ul className="-my-6 divide-y divide-gray-200 mt-1 border-4 border-green-500">
-                {productsCart.map((product, index) => (
+                {props.productsCart.map((product, index) => (
                   <li className="flex py-6">
                     <div
                       key={index}
@@ -105,6 +113,17 @@ export default function Header({ count, setCountUser, productsCart, setProductsC
               <span className="font-bold"> TOTAL: S/.{totalPrice}</span>
               <button>Pagar</button>
             </div>
+
+
+
+
+
+
+
+
+
+
+
           </div>
         </div>
       </header>
